89b4eb59e13361f81fcaacac36c605964a8bcf95
victor <jv.paixao.762.g@gmail.com>
2022-10-18 13:59:44 -0300

    feito

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1>Eu sou o APP</h1>
-      <Garagem nome="Victor"/>       
+     
     </div>
   );
 }
--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,16 +1,1 @@
-import React from "react";
-import Carro from "./Carro"
 
-function Garagem(props) {
-  return (
-    <div>
-      <h1>Garagem de {props.nome}</h1>
-      <Carro nome="Nissan " cor="verde" ano="2005" flex="GÁS, ÁLCOOL, GASOLINA" />
-      <Carro nome="Ford Mustang" cor="preto" ano="2021" flex="GASOLINA" />
-      <Carro nome="Porsche" cor="Azul perolado" ano="2020" flex="GÁS, ÁLCOOL" />
-      <Carro nome="BMW z4" cor="Cinza" ano="2018" flex="GASOLINA" />
-    </div>
-  )
-}
-
-export default Garagem
